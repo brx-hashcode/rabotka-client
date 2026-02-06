a7a25c6a6de170711cef688f150b64521be90da3
brx-nbif <blondeau.nbif@mail.ru>
2026-02-06 23:55:34 +0100

    chore: update package dependencies to include new libraries
    - Added `@t3-oss/env-core` version 0.13.10 for environment variable management.
    - Included `mvc-front-sdk` version 0.1.9 for enhanced SDK functionality.
    - Updated `pnpm-lock.yaml` to reflect the new dependencies and their versions.

---
+++ src/env.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  clientPrefix: "VITE_",
+
+  client: {
+    VITE_HOST: z.string().min(1),
+    VITE_PORT: z.string().min(1),
+    VITE_API_URL: z.string().url(),
+  },
+
+  runtimeEnv: import.meta.env,
+
+  emptyStringAsUndefined: true,
+});
